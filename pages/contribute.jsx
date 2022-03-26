ce1062e8cd0fa6089a03873ebca97628cf7ceae7
esot321c <86281023+esot321c@users.noreply.github.com>
2022-03-26 09:36:13 -0700

    Update contribute.jsx

--- pages/contribute.jsx
+++ pages/contribute.jsx
@@ -50,7 +50,7 @@ const EVENT_NAME = 'staker-paideia-202203';
 const ROUND_PROXY_NFT =
   '1a01a48318a3d747a728cb1844f30cebe4d198a62bc9a7876185880567efc5f6';
 const WHITELIST_TOKEN_ID =
-  'c114f1f25a6c892d741f99bb12353d9cf7644d448e13ac1da0eeb62817532ee4';
+  '8b70d5e59232d6437a74afec8d0eca20c00abaccd2518dbccf0ecde918b8831a';
 // const SIGUSD_TOKEN_ID =
 //   '03faf2cb329f2e90d6d23b58d91bbb6c046aa143261cc21f52fbe2824bfcbf04';
 // const PAIDEIA_TOKEN_ID = 'paideia_token_id';
