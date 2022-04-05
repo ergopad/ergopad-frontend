33c286e6e9f169e19508ff54d75046c663da3ed6
esot321c <esot321c@gmail.com>
2022-04-04 23:29:39 -0700

    :inprogress: update projects page

--- pages/projects/[project_id].jsx
+++ pages/projects/[project_id].jsx
@@ -6,6 +6,11 @@ import {
   Divider,
   Box,
   Grid,
+  List,
+  ListItemText,
+  ListItem,
+  ListItemIcon,
+  Icon
 } from '@mui/material';
 import CircularProgress from '@mui/material/CircularProgress';
 import Link from '@components/MuiNextLink';
@@ -20,6 +25,40 @@ import DiscordIcon from '@components/DiscordIcon';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import MarkdownRender from '@components/MarkdownRender';
+import Image from 'next/image'
+
+const navBarLinks = [
+  {
+    name: 'Description',
+    icon: 'info',
+    link: '#'
+  },
+  {
+    name: 'Roadmap',
+    icon: 'signpost',
+    link: '#'
+  },
+  {
+    name: 'Team',
+    icon: 'people',
+    link: '#'
+  },
+  {
+    name: 'Tokenomics',
+    icon: 'data_usage',
+    link: '#'
+  },
+  {
+    name: 'Distribution',
+    icon: 'toc',
+    link: '#'
+  },
+  {
+    name: 'Social',
+    icon: 'forum',
+    link: '#'
+  }
+]
 
 const Project = () => {
   const router = useRouter();
@@ -43,6 +82,13 @@ const Project = () => {
     if (project_id) getProject();
   }, [project_id]);
 
+  const listItemSx = {
+    borderRadius: '5px',
+    '&:hover': {
+      backgroundColor: 'rgba(255, 255, 255, 0.1)',
+    },
+  };
+
   return (
     <>
       {project ? (
@@ -55,120 +101,159 @@ const Project = () => {
                   position: 'relative',
                   left: '50%',
                   marginLeft: '-12px',
-                  marginTop: '120px',
+                  marginTop: '400px',
                 }}
               />
             </Container>
           )}
           {!isLoading && (
-            <Container maxWidth="760px" sx={{ maxWidth: '760px', mx: 'auto' }}>
-              <Box sx={{ mt: '3rem', mb: '3rem' }}>
-                <img
-                  src={project.bannerImgUrl}
-                  alt={project.name}
-                  width="100%"
-                />
-              </Box>
-              <Typography variant="h2">{project.name}</Typography>
-              <Typography variant="p">{project.shortDescription}</Typography>
-
-              <Divider sx={{ width: '2rem', mb: '1.5rem' }} />
-
-              {project.description && (
-                <>
-                  <Typography variant="h4" sx={{ mt: '2rem' }}>
-                    Description
-                  </Typography>
-                  <MarkdownRender description={project.description} />
-                </>
-              )}
-              {/* <Typography variant="h4">Meet the team</Typography>
-              {/* todo: Add rendering for team 
-              <Typography variant="p">
-                Update team description for content in this section.
-              </Typography> */}
-              {/* socials go here */}
-              <Grid container>
-                {project?.socials?.discord ? (
-                  <Link
-                    sx={{ display: 'flex', justifyContent: 'center' }}
-                    href={project.socials.discord}
-                    aria-label="discord"
-                    title="Discord"
-                    rel="noreferrer"
-                    target="_blank"
-                  >
-                    <IconButton aria-label="discord">
-                      <DiscordIcon />
-                    </IconButton>
-                  </Link>
-                ) : null}
-                {project?.socials?.github ? (
-                  <Link
-                    sx={{ display: 'flex', justifyContent: 'center' }}
-                    href={project.socials.github}
-                    aria-label="github"
-                    title="GitHub"
-                    rel="noreferrer"
-                    target="_blank"
-                  >
-                    <IconButton aria-label="github">
-                      <GitHubIcon />
-                    </IconButton>
-                  </Link>
-                ) : null}
-                {project?.socials?.telegram ? (
-                  <Link
-                    sx={{ display: 'flex', justifyContent: 'center' }}
-                    href={project.socials.telegram}
-                    aria-label="Telegram"
-                    title="Telegram"
-                    rel="noreferrer"
-                    target="_blank"
-                  >
-                    <IconButton aria-label="telegram">
-                      <TelegramIcon />
-                    </IconButton>
-                  </Link>
-                ) : null}
-                {project?.socials?.twitter ? (
-                  <Link
-                    sx={{ display: 'flex', justifyContent: 'center' }}
-                    href={project.socials.twitter}
-                    aria-label="twitter"
-                    title="Twitter"
-                    rel="noreferrer"
-                    target="_blank"
-                  >
-                    <IconButton aria-label="twitter">
-                      <TwitterIcon />
-                    </IconButton>
-                  </Link>
-                ) : null}
-                {project?.socials?.website ? (
-                  <Link
-                    sx={{ display: 'flex', justifyContent: 'center' }}
-                    href={project.socials.website}
-                    aria-label="website"
-                    title="Web"
-                    rel="noreferrer"
-                    target="_blank"
-                  >
-                    <IconButton aria-label="website">
-                      <PublicIcon />
-                    </IconButton>
-                  </Link>
-                ) : null}
-                <CopyToClipboard>
-                  {({ copy }) => (
-                    <IconButton
-                      aria-label="share"
-                      onClick={() => copy(window.location)}
+            <Container maxWidth="lg">
+              {/* <Box sx={{ mt: '3rem', mb: '3rem' }}>
+                    <img
+                      src={project.bannerImgUrl}
+                      alt={project.name}
+                      width="100%"
+                    />
+                  </Box> */}
+              <Grid
+                container
+                spacing={6}
+                sx={{
+                  mt: 1,
+                  justifyContent: 'space-between',
+                  // flexDirection: { xs: 'column', md: 'row' },
+                }}
+              >
+                <Grid item xs={12} md={3}>
+                  <Box sx={{ mb: '3rem', borderRadius: '150px', overflow: 'hidden' }}>
+                    <Image
+                      src="/paideia-test-square.png"
+                      alt="paideia"
+                      width={800}
+                      height={800}
+                    />
+                  </Box>
+                  <List>
+                  {navBarLinks.map(({ icon, name, link }, i) => (
+                    <ListItem
+                      button
+                      sx={{ ...listItemSx }}
+                      onClick={() => {link}}
                     >
-                      <ShareIcon />
-                    </IconButton>
+                      <ListItemIcon>
+                        <Icon>
+                          {icon}
+                        </Icon>
+                      </ListItemIcon>
+                      <ListItemText primary={name} />
+                    </ListItem>
+                  ))}
+                  </List>
+                </Grid>
+                <Grid item xs={12} md={9}>
+                  
+                  <Typography variant="h2">{project.name}</Typography>
+                  <Typography variant="p">{project.shortDescription}</Typography>
+
+                  <Divider sx={{ width: '2rem', mb: '1.5rem' }} />
+
+                  {project.description && (
+                    <>
+                      <Typography variant="h4" sx={{ mt: '2rem' }}>
+                        Description
+                      </Typography>
+                      <MarkdownRender description={project.description} />
+                    </>
                   )}
-                </CopyToClipboard>
+                  {/* <Typography variant="h4">Meet the team</Typography>
+                  {/* todo: Add rendering for team 
+                  <Typography variant="p">
+                    Update team description for content in this section.
+                  </Typography> */}
+                  {/* socials go here */}
+                  <Grid container>
+                    {project?.socials?.discord ? (
+                      <Link
+                        sx={{ display: 'flex', justifyContent: 'center' }}
+                        href={project.socials.discord}
+                        aria-label="discord"
+                        title="Discord"
+                        rel="noreferrer"
+                        target="_blank"
+                      >
+                        <IconButton aria-label="discord">
+                          <DiscordIcon />
+                        </IconButton>
+                      </Link>
+                    ) : null}
+                    {project?.socials?.github ? (
+                      <Link
+                        sx={{ display: 'flex', justifyContent: 'center' }}
+                        href={project.socials.github}
+                        aria-label="github"
+                        title="GitHub"
+                        rel="noreferrer"
+                        target="_blank"
+                      >
+                        <IconButton aria-label="github">
+                          <GitHubIcon />
+                        </IconButton>
+                      </Link>
+                    ) : null}
+                    {project?.socials?.telegram ? (
+                      <Link
+                        sx={{ display: 'flex', justifyContent: 'center' }}
+                        href={project.socials.telegram}
+                        aria-label="Telegram"
+                        title="Telegram"
+                        rel="noreferrer"
+                        target="_blank"
+                      >
+                        <IconButton aria-label="telegram">
+                          <TelegramIcon />
+                        </IconButton>
+                      </Link>
+                    ) : null}
+                    {project?.socials?.twitter ? (
+                      <Link
+                        sx={{ display: 'flex', justifyContent: 'center' }}
+                        href={project.socials.twitter}
+                        aria-label="twitter"
+                        title="Twitter"
+                        rel="noreferrer"
+                        target="_blank"
+                      >
+                        <IconButton aria-label="twitter">
+                          <TwitterIcon />
+                        </IconButton>
+                      </Link>
+                    ) : null}
+                    {project?.socials?.website ? (
+                      <Link
+                        sx={{ display: 'flex', justifyContent: 'center' }}
+                        href={project.socials.website}
+                        aria-label="website"
+                        title="Web"
+                        rel="noreferrer"
+                        target="_blank"
+                      >
+                        <IconButton aria-label="website">
+                          <PublicIcon />
+                        </IconButton>
+                      </Link>
+                    ) : null}
+                    <CopyToClipboard>
+                      {({ copy }) => (
+                        <IconButton
+                          aria-label="share"
+                          onClick={() => copy(window.location)}
+                        >
+                          <ShareIcon />
+                        </IconButton>
+                      )}
+                    </CopyToClipboard>
+                  </Grid>
+                </Grid>
               </Grid>
             </Container>
           )}
