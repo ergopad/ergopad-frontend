64bc79b5541dfff8d753f997981136ebed78c715
esot321c <esot321c@gmail.com>
2022-04-13 14:44:56 -0700

    remove unnecessary include

--- components/dashboard/AssetItem.jsx
+++ components/dashboard/AssetItem.jsx
@@ -1,7 +1,7 @@
 import { Typography, SvgIcon } from '@mui/material';
 import { styled } from '@mui/system';
 import AssetModal from './AssetModal';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 const SIGUSD_TOKEN_ID =
   '03faf2cb329f2e90d6d23b58d91bbb6c046aa143261cc21f52fbe2824bfcbf04';
