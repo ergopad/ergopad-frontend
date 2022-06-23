903a2f5923f9f5120ec7487fbe82df1fab4f7b6a
esot321c <esot321c@gmail.com>
2022-06-23 08:47:00 -0700

    add token page back

--- pages/token.jsx
+++ pages/token.jsx
@@ -102,7 +102,6 @@ const paperStyle = {
 };
 
 const Token = () => {
-    /*
     const [expanded, setExpanded] = useState(false);
     const [initialErgopadSupply, setInitialErgopadSupply] = useState('Loading...');
     const [currentErgopadSupply, setCurrentErgopadSupply] = useState('Loading...');
@@ -351,20 +350,6 @@ const Token = () => {
         <RelatedLinks title="Learn More" subtitle="" links={relatedLinkList} />
     </>
   );
-  */
- return (
-        <>
-            <Container maxWidth="lg" sx={{ mb: '3rem' }}>
-                <CenterTitle 
-                    title="Tokenomics"
-                    subtitle="For Ergo projects, are tokenomics called Ergonomics? "
-                    main={true}
-                />
-
-                Page will return soon, API is receiving maintenance. 
-            </Container>
-            </>
- )
 };
 
 export default Token;
