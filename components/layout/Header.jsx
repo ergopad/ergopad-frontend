a84250bc80458b9b4368e28048a8868b7d4bf343
esot321c <esot321c@gmail.com>
2023-07-11 20:02:23 -0700

    remove landing page modal

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -55,54 +55,54 @@ const Header = () => {
     setAddWalletOpen(true);
   };
 
-  const [modalOpen, setModalOpen] = useState(true);
+  // const [modalOpen, setModalOpen] = useState(true);
 
-  useEffect(() => {
-    localStorage.getItem("dontShowAgain") === "true" && setModalOpen(false)
-  }, []);
+  // useEffect(() => {
+  //   localStorage.getItem("dontShowAgain") === "true" && setModalOpen(false)
+  // }, []);
 
-  const BootstrapDialog = styled(Dialog)(({ theme }) => ({
-    '& .MuiDialogContent-root': {
-      padding: theme.spacing(2),
-    },
-    '& .MuiDialogActions-root': {
-      padding: theme.spacing(1),
-    },
-    '& .MuiPaper-root': {
-      backgroundImage: 'url("/ido-modal/spf-background.png")',
-      backgroundRepeat: 'no-repeat',
-      backgroundPositionX: 'center',
-      backgroundPositionY: 'center',
-    }
-  }));
+  // const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+  //   '& .MuiDialogContent-root': {
+  //     padding: theme.spacing(2),
+  //   },
+  //   '& .MuiDialogActions-root': {
+  //     padding: theme.spacing(1),
+  //   },
+  //   '& .MuiPaper-root': {
+  //     backgroundImage: 'url("/ido-modal/spf-background.png")',
+  //     backgroundRepeat: 'no-repeat',
+  //     backgroundPositionX: 'center',
+  //     backgroundPositionY: 'center',
+  //   }
+  // }));
 
-  function BootstrapDialogTitle(props) {
-    const { children, onClose, ...other } = props;
+  // function BootstrapDialogTitle(props) {
+  //   const { children, onClose, ...other } = props;
 
-    return (
-      <DialogTitle sx={{ m: 0, p: 2 }} {...other}>
-        {children}
-        {onClose ? (
-          <IconButton
-            aria-label="close"
-            onClick={onClose}
-            sx={{
-              position: 'absolute',
-              right: 8,
-              top: 8,
-              color: (theme) => theme.palette.grey[500],
-            }}
-          >
-            <CloseIcon />
-          </IconButton>
-        ) : null}
-      </DialogTitle>
-    );
-  }
+  //   return (
+  //     <DialogTitle sx={{ m: 0, p: 2 }} {...other}>
+  //       {children}
+  //       {onClose ? (
+  //         <IconButton
+  //           aria-label="close"
+  //           onClick={onClose}
+  //           sx={{
+  //             position: 'absolute',
+  //             right: 8,
+  //             top: 8,
+  //             color: (theme) => theme.palette.grey[500],
+  //           }}
+  //         >
+  //           <CloseIcon />
+  //         </IconButton>
+  //       ) : null}
+  //     </DialogTitle>
+  //   );
+  // }
 
-  const handleModalClose = () => {
-    setModalOpen(false);
-  };
+  // const handleModalClose = () => {
+  //   setModalOpen(false);
+  // };
 
   return (
     <>
