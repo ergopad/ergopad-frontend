da22c614a75a0f90dd3b2c6ba1a12b9ac5f0487d
esot321c <esot321c@gmail.com>
2024-05-08 15:22:51 -0700

    WIP: add contribute page

---
+++ components/projects/contribute/ContributeCard.tsx
@@ -0,0 +1,154 @@
+import { Avatar, Box, Button, Checkbox, FormControlLabel, Typography, useTheme } from '@mui/material';
+import React, { ChangeEvent, FC, useState } from 'react';
+import TokenInput from './TokenInput';
+import Link from '@components/Link';
+import ContributeConfirm from './ContributeConfirm';
+import { useWallet } from '@contexts/WalletContext';
+
+interface IContributeCardProps {
+  projectName: string;
+  projectIcon: string;
+  roundName: string;
+  tokenTicker: string;
+  remainingTokens: number;
+  exchangeRate: number;
+  whitelisted: boolean;
+  live: boolean;
+  contributionRoundId: number;
+  recipientAddress: string | null;
+  allowed: boolean;
+}
+
+const ContributeCard: FC<IContributeCardProps> = ({
+  projectName,
+  projectIcon,
+  roundName,
+  contributionRoundId,
+  tokenTicker,
+  remainingTokens,
+  exchangeRate,
+  whitelisted,
+  live,
+  recipientAddress,
+  allowed
+}) => {
+  const { sessionData } = useWallet()
+
+  const theme = useTheme()
+  const [termsCheck, setTermsCheck] = useState(false)
+  const [openContribution, setOpenContribution] = useState(false)
+  const [inputValue, setInputValue] = useState('');
+  const [outputValue, setOutputValue] = useState('');
+
+  const handleCheckTerms = (e: ChangeEvent) => {
+    setTermsCheck(!termsCheck)
+  }
+
+  return (
+    <Box sx={{
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      height: '100%',
+      justifyContent: 'space-between'
+    }}>
+      <Box sx={{ textAlign: 'center' }}>
+        <Typography variant="h3" sx={{ mb: 1 }}>
+          {projectName}
+        </Typography>
+        <Typography variant="h4" sx={{ mb: 3 }}>
+          {roundName} contribution
+        </Typography>
+      </Box>
+      <Box sx={{ mb: 2 }}>
+        <Box sx={{ mb: 2 }}>
+          <TokenInput
+            outputTokenTicker={tokenTicker}
+            remainingTokens={remainingTokens}
+            exchangeRate={exchangeRate}
+            inputValue={inputValue}
+            setInputValue={setInputValue}
+            outputValue={outputValue}
+            setOutputValue={setOutputValue}
+          />
+        </Box>
+        <FormControlLabel
+          control={
+            <Checkbox checked={termsCheck} onChange={handleCheckTerms} name="terms" size="small" color="secondary" />
+          }
+          sx={{ mb: 2 }}
+          label={<Typography sx={{ fontSize: '1rem !important', color: theme.palette.text.secondary }}>
+            I agree to the&nbsp;<Link href="/terms" target="_blank">Terms of Use</Link> and <Link href="/privacy" target="_blank">Privacy Policy</Link>.
+          </Typography>}
+        />
+      </Box>
+      <Box sx={{ display: 'flex', justifyContent: 'center', flexDirection: 'column' }}>
+        <Box sx={{ width: '100%', textAlign: 'center' }}>
+          <Button
+            variant="contained"
+            color="secondary"
+            disabled={
+              !termsCheck ||
+              !whitelisted ||
+              !live ||
+              !recipientAddress ||
+              Number(inputValue) === 0 ||
+              sessionData == null ||
+              !allowed
+            }
+            sx={{
+              textTransform: 'none',
+              fontSize: '20px',
+              fontWeight: 600,
+              borderRadius: '6px'
+            }}
+            onClick={() => setOpenContribution(true)}
+          >
+            Contribute now
+          </Button>
+        </Box>
+        {!recipientAddress &&
+          <Typography color="error" sx={{ mt: 1, fontSize: '0.9rem!important' }}>
+            Contribution form error, please notify support if you are trying to contribute.
+          </Typography>
+        }
+        {!allowed &&
+          <Typography color="error" sx={{ mt: 1, fontSize: '0.9rem!important' }}>
+            We apologize, but we are unable to accept contributions from the restricted countries listed above.
+          </Typography>
+        }
+        {sessionData == null &&
+          <Box sx={{ textAlign: 'center' }}>
+            <Typography color="error" sx={{ mt: 1, fontSize: '0.9rem!important' }}>
+              Please sign in to contribute
+            </Typography>
+            <Typography sx={{ mt: 1, fontSize: '0.9rem!important' }}>
+              The wallet you send funds from doesn&apos;t need to be the same wallet you sign-in from.
+            </Typography>
+            <Typography sx={{ mt: 1, fontSize: '0.9rem!important' }}>
+              LEDGER USERS: Sign in with another wallet then contribute from your Ledger.
+            </Typography>
+          </Box>
+        }
+      </Box>
+      {!whitelisted && <Box>
+        <Typography color="text.secondary" sx={{ fontSize: '0.9rem!important', fontStyle: 'italic' }}>
+          You must be whitelisted to contribute.
+        </Typography>
+      </Box>}
+      {recipientAddress !== null &&
+        <ContributeConfirm
+          open={openContribution}
+          setOpen={setOpenContribution}
+          paymentAmount={inputValue}
+          receiveAmount={outputValue}
+          receiveCurrency={tokenTicker}
+          contributionRoundId={contributionRoundId}
+          recipientAddress={recipientAddress}
+        />
+      }
+    </Box>
+  );
+};
+
+export default ContributeCard;
---
+++ components/projects/contribute/TokenInput.tsx
@@ -0,0 +1,196 @@
+import { Box, InputBase, Paper, Typography, useTheme } from '@mui/material';
+import React, { FC, useEffect, useState } from 'react';
+import { trpc } from '@utils/trpc';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+interface ITokenInputProps {
+  inputTokenTicker?: string;
+  outputTokenTicker: string;
+  remainingTokens: number;
+  exchangeRate: number; // 1 input token = (exchangeRate * input) output tokens
+  inputValue: string;
+  setInputValue: React.Dispatch<React.SetStateAction<string>>;
+  outputValue: string;
+  setOutputValue: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const TokenInput: FC<ITokenInputProps> = ({
+  inputTokenTicker,
+  outputTokenTicker,
+  remainingTokens,
+  exchangeRate,
+  inputValue,
+  setInputValue,
+  outputValue,
+  setOutputValue
+}) => {
+  const theme = useTheme();
+  const { data: adaPrice } = trpc.price.getCardanoPrice.useQuery();
+  const [adaAmount, setAdaAmount] = useState<number | undefined>(undefined);
+
+  const getUserAdaAmount = async () => {
+    try {
+      if (connected) {
+        const lovelace = await wallet.getLovelace();
+        const ada = Number(lovelace) * 0.000001;
+        if (ada) setAdaAmount(ada);
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  };
+
+  useEffect(() => {
+    if (wallet) {
+      getUserAdaAmount();
+    }
+  }, [wallet]);
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const rawValue = event.target.value.replace(/,/g, '.');
+
+    // Function to count the number of periods in the string
+    const countPeriods = (str: string) => (str.match(/\./g) || []).length;
+
+    // Only update the input value if it doesn't result in multiple periods
+    if (countPeriods(rawValue) <= 1) {
+      setInputValue(rawValue);
+
+      // Convert to a number for output value, handling potential NaN
+      const numericValue = Number(rawValue);
+      if (!isNaN(numericValue)) {
+        setOutputValue((numericValue * exchangeRate).toFixed(0));
+      } else {
+        setOutputValue('');
+      }
+    }
+  };
+
+  const handleInputMax = () => {
+    if (adaAmount) {
+      const roundedDown = adaAmount - 1.5
+      setInputValue(roundedDown.toFixed(0))
+      setOutputValue((Number(roundedDown) * exchangeRate).toFixed(0))
+    }
+  }
+
+  const calculateUSDValue = () => {
+    const numericalValue = Number(inputValue.replace(/,/g, ''));
+    return (numericalValue * (adaPrice || 0)).toLocaleString(undefined, { maximumFractionDigits: 2 });
+  };
+
+  return (
+    <Box sx={{ position: 'relative' }}>
+      <Box
+        sx={{
+          border: `1px solid ${theme.palette.divider}`,
+          width: '100%',
+          py: 1,
+          px: 2,
+          mb: 1,
+          maxWidth: '400px',
+          borderRadius: '8px',
+          background: theme.palette.mode === 'dark' ? 'rgba(235,245,255,0.03)' : 'rgba(255,255,255,0.55)'
+        }}
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-between'
+          }}>
+          <InputBase
+            sx={{
+              '& .MuiInputBase-input': {
+                fontSize: '32px',
+                fontWeight: 700
+              }
+            }}
+            placeholder={"0.00"}
+            value={inputValue}
+            onChange={handleInputChange}
+          />
+          <Typography sx={{
+            fontSize: '26px!important', fontWeight: 700, whiteSpace: 'nowrap',
+          }}>
+            ADA ₳
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-between'
+          }}>
+          <Typography sx={{ fontSize: '1rem!important' }}>
+            ${calculateUSDValue()}
+          </Typography>
+          <Typography sx={{ fontSize: '1rem!important', whiteSpace: 'nowrap' }}>
+            Balance:&nbsp;
+            {adaAmount
+              ? <Box component="span" onClick={handleInputMax} sx={{ cursor: 'pointer' }}>
+                {adaAmount.toLocaleString(undefined, { maximumFractionDigits: 2 })}
+              </Box>
+              : '0'}
+          </Typography>
+        </Box>
+      </Box>
+      <Box
+        sx={{
+          border: `1px solid ${theme.palette.divider}`,
+          width: '100%',
+          py: 1,
+          px: 2,
+          maxWidth: '400px',
+          borderRadius: '8px',
+          background: theme.palette.mode === 'dark' ? 'rgba(235,245,255,0.03)' : 'rgba(255,255,255,0.55)'
+        }}
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-between'
+          }}>
+          <InputBase
+            sx={{
+              '& .MuiInputBase-input': {
+                fontSize: '32px',
+                fontWeight: 700
+              }
+            }}
+            placeholder={"0"}
+            value={outputValue}
+          />
+          <Typography sx={{
+            fontSize: '26px!important', fontWeight: 700, whiteSpace: 'nowrap',
+          }}>
+            {outputTokenTicker}
+          </Typography>
+        </Box>
+        <Typography sx={{ fontSize: '1rem!important' }}>
+          {remainingTokens.toLocaleString(undefined, { maximumFractionDigits: 0 })} {outputTokenTicker} remaining
+        </Typography>
+      </Box>
+      <Paper
+        variant="outlined"
+        sx={{
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%,-50%)',
+          zIndex: 5,
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}
+      >
+        <ExpandMoreIcon />
+      </Paper>
+    </Box>
+  );
+};
+
+export default TokenInput;
